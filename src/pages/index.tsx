1624974d98b82affa49935681d60ea3841102e61
Nicolas Aguiar <nicolas98aguiar@gmail.com>
2021-04-25 19:14:30 -0300

    Inclusão de imagens na homepage da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { GetStaticProps } from 'next';
-import {format, parseISO} from 'date-fns';
+import Image from 'next/image';
+import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
-import { convertDurantionToTimeString } from '../utils/convertDurantionToTimeString';
+import { convertDurationToTimeString as convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import styles from './home.module.scss';
 
 type Episode = {
   id: string;
@@ -18,44 +20,87 @@ type Episode = {
 };
 
 type HomeProps = {
-  episodes: Episode[]; //or Array<Episode>
+  allEpisodes: Episode[];
+  latestEpisodes: Episode[];
+
+  //or Array<Episode>
 };
 
 // Usando o useEffect eu estou usando o SPA.
-export default function Home(props: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
   return (
-    <h1>Index</h1>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {
+            latestEpisodes.map((episode, key) => {
+              return (
+                <li key={key}>
+                  <Image
+                    width={192}
+                    height={192}
+                    src={episode.thumbnail}
+                    alt={episode.title}
+                    objectFit="cover"
+                  />
+                  <div className={styles.episodesDetails}>
+                    <a href="">{episode.title}</a>
+                    <p>{episode.members}</p>
+                    <span>{episode.published_at}</span>
+                    <span>{episode.durationAsString}</span>
+                  </div>
+
+                  <button type="button">
+                    <img src="/play-green.svg" alt="Tocar episodio" />
+                  </button>
+                </li>
+              )
+            })
+          }
+        </ul>
+
+      </section>
+      <section className={styles.allEpisodes}>
+
+      </section>
+    </div>
   );
 };
 
 // Usando SSG para buscar os dados da API
 export const getStaticProps: GetStaticProps = async () => {
-  const {data} = await api.get('episodes', {
+  const { data } = await api.get('episodes', {
     params: {
-      _limit:12,
+      _limit: 12,
       _sort: 'published_at',
       _order: 'desc',
     }
   });
-  
+
   const episodes = data.map(episode => {
     return {
       id: episode.id,
       title: episode.title,
       thumbnail: episode.thumbnail,
       members: episode.members,
-      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
       duration: Number(episode.file.duration),
-      durationAsString: convertDurantionToTimeString(episode.file.duration),      
+      durationAsString: convertDurationToTimeString(episode.file.duration),
       description: episode.description,
-      url: episode.fiel.url,
+      url: episode.file.url,
     }
   });
 
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
   return {
     props: {
-      episodes: data,
+      latestEpisodes,
+      allEpisodes
     },
     revalidate: 60 * 60 * 8, // revalidando a API a cada 8 horas.
   }
--- src/utils/convertDurationToTimeString.ts
+++ src/utils/convertDurationToTimeString.ts
@@ -1,4 +1,4 @@
-export function convertDurantionToTimeString(duration: number){
+export function convertDurationToTimeString(duration: number){
     const hours = Math.floor(duration / 3600);
     const minutes = Math.floor((duration % 3600) / 60);
     const seconds = duration % 60;
